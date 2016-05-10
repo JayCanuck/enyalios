d7da8acd4e45e3c0a4e44e0239e2c9ca77464aa9
Jason Robitaille <robitaille88@gmail.com>
2016-05-09 19:26:05 -0700

    Dynamic basicground with basic app management

--- src/EnyaliOS.js
+++ src/EnyaliOS.js
@@ -1,18 +1,32 @@
 var
+	AppMgr = require('./data/AppMgr'),
+	SysPrefs = require('./data/SysPrefs'),
 	Control = require('enyo/Control'),
 	StatusBar = require('./ui/StatusBar'),
 	SearchBar = require('./ui/SearchBar'),
 	GestureBar = require('./ui/GestureBar');
+
+var USER_MEDIA = '@../system/user/media/';
 
 module.exports = Control.kind({
 	name: 'EnyaliOS',
 	fit:true,
 	style:'background:black; display:flex; flex-direction: column;',
 	components: [
+		{kind:AppMgr, onAppsChanged:''},
+		{kind:SysPrefs, onWallpaperUpdated:'updateWallpaper', onWallpaperFailure:''},
 		{kind:StatusBar},
-		{style:"flex:1;border-radius:1.5rem;background: url('@../assets/wallpapers/01.jpg') no-repeat center center; background-size: cover;", components: [
-			{kind:SearchBar}
+		{name:'bg', style:'flex:1;border-radius:1.5rem; display:flex; flex-direction: column;', components: [
+			{kind:SearchBar},
+			{style:'flex:1', ontap:'handleTap'}
 		]},
 		{kind:GestureBar}
-	]
+	],
+	create: function() {
+		this.inherited(arguments);
+		this.$.bg.applyStyle('background', 'url(\'' + this.$.sysPrefs.getWallpaper() + '\') center center / cover no-repeat');
+	},
+	updateWallpaper: function(inSender, inEvent) {
+		this.$.bg.applyStyle('background', 'url(\'' + inEvent.wallpaper + '\') center center / cover no-repeat');
+	}
 });
